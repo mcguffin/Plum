ddee2aa5fe59037098085cdc97d23fa1965523e2
Jörn Lund <joern@flyingletters.com>
2015-11-03 14:00:12 +0100

    remove draw observer

--- Plum.roboFontExt/lib/plum/observer.py
+++ Plum.roboFontExt/lib/plum/observer.py
@@ -7,7 +7,6 @@ class PlumObserver(object):
 
     def __init__(self):
         addObserver(self, 'update', 'currentGlyphChanged')
-        addObserver(self, 'update', 'draw')
 
     def update(self, info):
         Plum(info['glyph']).update()
